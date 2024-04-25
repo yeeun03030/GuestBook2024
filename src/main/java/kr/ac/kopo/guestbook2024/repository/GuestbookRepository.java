c83d6f158fd8172646827c52ec6c5371648c35c6
yeeun03030 <skdPdms04@gmail.com>
2024-04-25 11:49:08 +0900

    JPA와 QueryDSL

--- src/main/java/kr/ac/kopo/guestbook2024/repository/GuestbookRepository.java
+++ src/main/java/kr/ac/kopo/guestbook2024/repository/GuestbookRepository.java
@@ -2,6 +2,7 @@ package kr.ac.kopo.guestbook2024.repository;
 
 import kr.ac.kopo.guestbook2024.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
 }
