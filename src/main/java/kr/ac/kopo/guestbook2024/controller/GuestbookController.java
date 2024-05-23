ba4bccfd31902cf9f717a5063a4bd322e4e573f8
yeeun03030 <skdPdms04@gmail.com>
2024-05-23 11:39:49 +0900

    Controller계층과 View계층: 컨트롤러 list.html 수정

--- src/main/java/kr/ac/kopo/guestbook2024/controller/GuestbookController.java
+++ src/main/java/kr/ac/kopo/guestbook2024/controller/GuestbookController.java
@@ -1,15 +1,28 @@
 package kr.ac.kopo.guestbook2024.controller;
 
+import kr.ac.kopo.guestbook2024.dto.PageRequestDTO;
+import kr.ac.kopo.guestbook2024.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list() {
-        return "/guestbook/list";
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping({"/list"})
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        // list.html(View 계층)에 방명록 목록과 화면에 보여질 때 필요한 페이지번호들 등의 정보를 전달하기 위함
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
